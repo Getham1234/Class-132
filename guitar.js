7fe6d21821599d8f7db768a0e4413f4adb88d6e7
Getham1234 <73149122+Getham1234@users.noreply.github.com>
2021-09-22 21:52:27 -0700

    Add files via upload

---
+++ guitar.js
@@ -0,0 +1,37 @@
+function back(){
+    window.location.replace("index.html");
+}
+
+img = "";
+status = "";
+
+function preload(){
+    img = loadImage("guitar.jpeg");
+}
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded")
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+    }
+}
